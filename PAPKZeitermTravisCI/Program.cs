0f38cad71942a6037f4d742a5c85f9a45b2bf75a
FaMisch <74175465+FaMisch@users.noreply.github.com>
2021-03-24 15:07:40 +0100

    Add files via upload

---
+++ PAPKZeitermTravisCI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PAPKZeitermTravisCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
